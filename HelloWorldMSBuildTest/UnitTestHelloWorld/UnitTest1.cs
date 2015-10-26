8384c8fa774f61c568297409f8504231f13a1d3a
Mathas Poulsen <mathias-poulsen@hotmail.com>
2015-10-26 10:59:38 +0100

    Hello!

---
+++ HelloWorldMSBuildTest/UnitTestHelloWorld/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestHelloWorld
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
